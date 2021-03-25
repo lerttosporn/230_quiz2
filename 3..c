2b1b5ff618b4659f858ad777a6e1efd9bbd2374e
lerttosprorn <lerttosporn.s@ku.th>
2021-03-25 19:52:14 +0700

    palindom

--- 3..c
+++ 3..c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,m,k,l,tmp1=0,tmp2=0,chk=0;
+    int n,i,m,k,l,tmp1=0,tmp2=0,chk=0,cnt;
     scanf("%d",&n);
     for(k=1; k<n; k++)
-    {m=k;
-        for(i=0; k!=1; i++)
+    {   cnt=k;
+        m=k;
+        for(i=0; cnt!=0; i++)
         {
-            k=k/10;
+            cnt=cnt/10;
         }
-        i++;
         int ar[i],pa[i],j;
-        for(j=0; j<i; j++)
+        for(j=0; m!=0; j++)
         {
-            ar[i]=m%10;
+            ar[j]=m%10;
             m=m/10;
         }
         for(l=j; l>0; l--)
@@ -29,11 +29,12 @@ int main()
         }
         tmp1=tmp1/10;
         tmp2=tmp2/10;
-        printf("%d %d",tmp1,tmp2);
         if(tmp1==tmp2)
         {
             chk++;
         }
+        tmp1=0;
+        tmp2=0;
     }
     printf("%d",chk);
     return 0;
