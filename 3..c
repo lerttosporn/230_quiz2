6ab8c5885f1413a0738ab4a88d67e49e3a8b034f
lerttosprorn <lerttosporn.s@ku.th>
2021-03-25 18:32:15 +0700

    palin

--- 3..c
+++ 3..c
@@ -4,24 +4,23 @@ int main()
     int n,i,m,k,l,tmp1=0,tmp2=0,chk=0;
     scanf("%d",&n);
     for(k=1; k<n; k++)
-    {
+    {m=k;
         for(i=0; k!=1; i++)
         {
             k=k/10;
         }
         i++;
-        m=k;
         int ar[i],pa[i],j;
         for(j=0; j<i; j++)
         {
             ar[i]=m%10;
             m=m/10;
         }
-        for(l=j;l>0; l--)
+        for(l=j; l>0; l--)
         {
             pa[l-1]=ar[j-l];
         }
-        for(j=0;j<i;j++)
+        for(j=0; j<i; j++)
         {
             tmp1=tmp1+ar[j];
             tmp1=tmp1*10;
@@ -30,6 +29,7 @@ int main()
         }
         tmp1=tmp1/10;
         tmp2=tmp2/10;
+        printf("%d %d",tmp1,tmp2);
         if(tmp1==tmp2)
         {
             chk++;
